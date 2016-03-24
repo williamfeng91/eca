4b89a01fa3be8cfa8dce007928f0743e6bdcd24a
William Feng <williamf@webrecs.com.au>
2016-03-24 17:58:17 +1100

    Changed server file location

--- server/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-app = express();
-
-app.use(express.static(__dirname));
-app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname })
-});
-var server = app.listen(process.env.PORT || 8000);
